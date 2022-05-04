42c76e31c4b2036ee8343877935a5cb719de32a2
khanhvo <vodk23@#>
2022-05-04 11:36:55 +0700

    Fix bugs

--- public/class-fathom-analytics-conversions-public.php
+++ public/class-fathom-analytics-conversions-public.php
@@ -43,8 +43,8 @@ class Fathom_Analytics_Conversions_Public {
 	 * Initialize the class and set its properties.
 	 *
 	 * @since    1.0.0
-	 * @param    string    $plugin_name       The name of the plugin.
-	 * @param    string    $version    The version of this plugin.
+	 * @param    string $plugin_name       The name of the plugin.
+	 * @param    string $version    The version of this plugin.
 	 */
 	public function __construct( $plugin_name, $version ) {
 
